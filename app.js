48c297b1fb449034f7dd022d11cfe396bae7ddd9
stevieFrog <(steve@steve-merchant.co.uk>
2023-01-18 14:53:07 +0000

    updated PW meter

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ document.addEventListener('DOMContentLoaded', () => {
             } else {
                 outputValue = nMinus1 + nMinus2;
                 console.log(outputValue);
-                nMinus2 = nMinus1
+                nMinus2 = nMinus1;
                 nMinus1 = outputValue;
             }
         }
@@ -72,5 +72,92 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById("fibSequenceLength").addEventListener("change", function() {
         fibSequence(fibSequenceStart.value, this.value);
     })
+
+    function coffeeWaterRatio (coffeeRatio, coffeeMass) {
+        console.log(coffeeRatio, coffeeMass);
+        const waterMass = coffeeRatio * coffeeMass;
+        document.write('<h2>Coffee</h2><br>For a 1:' + coffeeRatio + ' Ratio with ' + coffeeMass + 'g coffee<br>You will need ' + waterMass + ' grams of water<br>');
+    }
+
+    document.getElementById("coffeeMass").addEventListener("change", function() {
+        coffeeWaterRatio (coffeeRatio.value, coffeeMass.value);
+    })
+
+    function calculatePasswordEntropy (password) {
+        const passwordArray = password.split("")
+        const regExp_az = /[a-z]/
+        const regExp_AZ = /[A-Z]/
+        const regExp_digits = /[0-9]/
+        const regExp_special = /[^a-z^A-Z^0-9]]/
+        let lowerCaseLatinPool=26
+        let upperCaseLatinPool=26
+        let digitsPool=10
+        let specialPool=32
+        var lowerCaseLatinCount=0
+        var upperCaseLatinCount=0
+        var digitsCount=0
+        var specialCount=0
+        let totalPool=0
+        let passwordEntropy = 0
+        let guessesRequired = 0
+        let smallGuessCount = 1000
+        let mediumGuessCount = 100000000000
+        let largeGuessCount = 100000000000000
+        let passwordStrength = ''
+
+        for(i=0; i < passwordArray.length; i++) {
+            if (passwordArray[i].match(regExp_az)) {
+                lowerCaseLatinCount++
+            }
+            else if (passwordArray[i].match(regExp_AZ)) {
+                upperCaseLatinCount++
+            }
+            else if (passwordArray[i].match(regExp_digits)) {
+                digitsCount++
+            }
+            else specialCount++
+        }
+        
+        if(lowerCaseLatinCount > 0) totalPool += lowerCaseLatinPool
+        if(upperCaseLatinCount > 0) totalPool += upperCaseLatinPool
+        if(digitsCount > 0) totalPool += digitsPool
+        if(specialCount > 0) totalPool += specialPool
+        
+        passwordEntropy = passwordArray.length * Math.log2(totalPool)
+        passwordEntropyRounded = mathRound(passwordEntropy,3)
+        guessesRequired = mathRound(Math.pow(2, passwordEntropy), 0)
+
+        if (passwordEntropy<10) {
+            passwordStrength = "YOU'VE ALREADY BEEN HACKED"
+        } else if(passwordEntropy<15){
+            passwordStrength = 'VERY POOR'
+        } else if(passwordEntropy<25) {
+            passwordStrength = 'POOR'
+        } else if(passwordEntropy<50) {
+            passwordStrength = 'WEAK'
+        } else if(passwordEntropy<75) {
+            passwordStrength = 'MEDIUM'
+        } else if(passwordEntropy<100){
+            passwordStrength = 'GOOD'
+        } else if(passwordEntropy<150){
+            passwordStrength = 'VERY STRONG'
+        } else if(passwordEntropy<200){
+            passwordStrength = 'VERY VERY STRONG'
+        } else passwordStrength = 'CRAZY STRONG'
+
+
+     /*   console.log(password);
+        console.log(passwordArray);
+        console.log(lowerCaseLatinCount, upperCaseLatinCount, digitsCount, specialCount, totalPool);
+        console.log("Password Entropy: " + passwordEntropy)
+    */
+        document.write('Password Contains <br> ' + lowerCaseLatinCount + ' a-z characters <br> ' + upperCaseLatinCount + ' A-Z characters <br> ' + digitsCount + ' 0-9 <br> ' + specialCount + ' Special characters<br><br> Password length is ' + passwordArray.length + '<br>The size of the character pool is:  ' + totalPool + '<br><h2>Password Entropy is: ' + passwordEntropyRounded + '</h2><h2>The password strength is: '+ passwordStrength + '</h2> This would require approx ' + guessesRequired + ' guesses <br><br>At '+ smallGuessCount + ' guesses per second this would take approximately <br>' + mathRound(guessesRequired / smallGuessCount,0) + ' Seconds to crack<br>' +  mathRound(guessesRequired / (smallGuessCount * 86400),1) + ' Days to crack<br>' + mathRound(guessesRequired / (smallGuessCount * 86400 * 365),2) + ' Years to crack<br>' + mathRound(guessesRequired / (smallGuessCount * 86400 * 365 * 10),2) + ' Decades to crack<br><br>At '+ mediumGuessCount + ' guesses per second this would take approximately <br>' + mathRound(guessesRequired / mediumGuessCount,0) + ' Seconds to crack<br>' +  mathRound(guessesRequired / (mediumGuessCount * 86400),1) + ' Days to crack<br>' + mathRound(guessesRequired / (mediumGuessCount * 86400 * 365),2) + ' Years to crack<br>' + mathRound(guessesRequired / (mediumGuessCount * 86400 * 365 * 10),2) + ' Decades to crack<br><br>At '+ largeGuessCount + ' guesses per second this would take approximately <br>' + mathRound(guessesRequired / largeGuessCount,0) + ' Seconds to crack<br>' +  mathRound(guessesRequired / (largeGuessCount * 86400),1) + ' Days to crack<br>' + mathRound(guessesRequired / (largeGuessCount * 86400 * 365),2) + ' Years to crack<br>' + mathRound(guessesRequired / (largeGuessCount * 86400 * 365 * 10),2) + ' Decades to crack<br>');
+        
+    }
+
+    document.getElementById("passwordForCalculation").addEventListener("change", function() {
+        calculatePasswordEntropy (passwordForCalculation.value);
+    })
+
 
 })
